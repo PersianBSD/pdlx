21dd23bba02d63f493a356b289176aaba6cc8cad
pdlinux <ali1354.asadi@gmail.com>
2025-08-20 01:01:06 +0330

    pdlx repo upload

---
+++ x86_64/update-pdlx-repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# مسیر مخزن
+REPO_DIR="/home/$USER/repo/pdlx/x86_64"
+
+REPO_NAME="pdlx"
+
+cd "$REPO_DIR" || { echo "❌ Cannot access $REPO_DIR"; exit 1; }
+
+# حذف دیتابیس قدیمی
+rm -f "${REPO_NAME}.db" "${REPO_NAME}.files"
+
+# ایجاد دیتابیس جدید
+repo-add "${REPO_NAME}.db.tar.zst" ./*.pkg.tar.zst
+
+echo "✅ $REPO_NAME Repo updated succesfully "
